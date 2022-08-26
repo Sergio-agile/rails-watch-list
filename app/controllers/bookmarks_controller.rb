40f7df12d00b4813122ccd8f8b36b4d64a50ef5c
sergio garcia <sergio.garcia.sanjuan@gmail.com>
2022-08-26 09:02:26 +0200

    creating a bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,10 +3,9 @@ class BookmarksController < ApplicationController
   def create
     debugger
     @list = List.find(params[:list_id])
-    @movie = Movie.find(params[:movie_id])
-    @bookmark.list = @list
-    @bookmark.movie = @movie
-    @bookmark = Bookmark.new(bookmark_params)
+    @movie = Movie.find(params[:bookmark][:movie])
+    @comment = params[:bookmark][:comment]
+    @bookmark = Bookmark.new(comment: @comment, movie: @movie, list: @list)
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -17,6 +16,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment).permit(:name)
+    params.require(:bookmark).permit(:comment, :movie, :list)
   end
 end
